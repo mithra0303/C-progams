75a02c5d63106722b2a2114e6856727f812bf24b
k.Sangamithra <106364573+mithra0303@users.noreply.github.com>
2023-12-30 09:52:03 +0530

    Update 1d.c

--- 1d.c
+++ 1d.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    // Declare variables
+
     float fahrenheit, celsius;
 
-    // Input temperature in Fahrenheit
+    
     printf("Enter temperature in Fahrenheit: ");
     scanf("%f", &fahrenheit);
 
-    // Convert Fahrenheit to Celsius
+    
     celsius = (fahrenheit - 32) * 5 / 9;
 
-    // Display the result
+    
     printf("Temperature in Celsius: %.2f\n", celsius);
 
     return 0;
