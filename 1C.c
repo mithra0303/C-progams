4e09e30f428b219226507599bb8b448727780b0c
k.Sangamithra <106364573+mithra0303@users.noreply.github.com>
2023-12-30 09:53:21 +0530

    Update 1C.c

--- 1C.c
+++ 1C.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to input marks for five subjects
+
 void inputMarks(int marks[]) {
     printf("Enter marks for five subjects (out of 100):\n");
 
@@ -8,7 +8,7 @@ void inputMarks(int marks[]) {
         printf("Subject %d: ", i + 1);
         scanf("%d", &marks[i]);
 
-        // Validate input marks (between 0 and 100)
+        
         if (marks[i] < 0 || marks[i] > 100) {
             printf("Invalid marks. Marks should be between 0 and 100.\n");
             // Exit the program with an error code
@@ -17,12 +17,12 @@ void inputMarks(int marks[]) {
     }
 }
 
-// Function to calculate aggregate marks
+
 float calculateAggregateMarks(int marks[]) {
     return marks[0] + marks[1] + marks[2] + marks[3] + marks[4];
 }
 
-// Function to calculate percentage
+
 float calculatePercentage(float aggregateMarks) {
     return (aggregateMarks / (5 * 100)) * 100;
 }
@@ -31,16 +31,16 @@ int main() {
     int marks[5];
     float aggregateMarks, percentage;
 
-    // Input marks for five subjects
+    
     inputMarks(marks);
 
-    // Calculate aggregate marks
+    
     aggregateMarks = calculateAggregateMarks(marks);
 
-    // Calculate percentage
+    
     percentage = calculatePercentage(aggregateMarks);
 
-    // Display the aggregate marks and percentage
+
     printf("Aggregate Marks: %.2f\n", aggregateMarks);
     printf("Percentage: %.2f%%\n", percentage);
 
